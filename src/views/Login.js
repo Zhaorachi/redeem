ce0a4b7dc38ba880a80e7223a92691f7a77e81ff
Zhaorachi0102 <silvertiger0321@gmail.com>
2021-02-04 21:20:05 +0800

    API URL

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './Login.css'
 
 import apiClient from "../Services/ApiClient";
 
-export default class Login extends Component{
+export default class Login extends Component {
 
-  state = {
-    email: '',
-  }
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const {
-      email
-    } = this.state;
-    console.log('it works', email);
-    apiClient
-    .login({
-        email: email
-    })
-    .catch((err) =>{
-      console.log(err)
-    });
-  };
+    state = {
+        email: '',
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {
+            email
+        } = this.state;
+        console.log('it works', email);
+        apiClient.login({email: email})
+            .then(res => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
 
-  render(){
+    render() {
 
-    return(
-      <div className='loginBox'>
-        <h1>Login</h1>
-        <br/>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="emailPayer"
-            placeholder="jhondoe@gmail.com"
-            onChange={this.handleChange}
-          />
-          <br/>
-          <input
-            type='submit'
-            value='Login'
-          />
-          </form>
-      </div>
-    )
-  }
+        return (
+            <div className='loginBox'>
+                <h1>Login</h1>
+                <br/>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="emailPayer"
+                        placeholder="jhondoe@gmail.com"
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <input
+                        type='submit'
+                        value='Login'
+                    />
+                </form>
+            </div>
+        )
+    }
 }
