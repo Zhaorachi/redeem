c013554dd25b3529a7d826358e897080e791b56b
Zhaorachi0102 <silvertiger0321@gmail.com>
2021-02-04 22:04:19 +0800

    Display response for API

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,6 +7,8 @@ export default class Login extends Component {
 
     state = {
         email: '',
+        cname: '',
+        message: '',
     }
     handleChange = (e) => {
         this.setState({
@@ -21,7 +23,10 @@ export default class Login extends Component {
         console.log('it works', email);
         apiClient.login({email: email})
             .then(res => {
-                console.log(res);
+                const status = res.data.status;
+                if(status === 200) this.setState({cname: 'success', message: res.data.message});
+                else if(status === 500) this.setState({cname: 'error', message: res.data.message});
+                else this.setState({cname: 'warning', message: res.data.message});
             })
             .catch((err) => {
                 console.log(err)
@@ -29,10 +34,15 @@ export default class Login extends Component {
     };
 
     render() {
-
+        const { cname, message } = this.state;
         return (
             <div className='loginBox'>
                 <h1>Login</h1>
+                {cname !== "" &&
+                    <div className={cname}>
+                        {message}
+                    </div>
+                }
                 <br/>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="email">Email</label>
